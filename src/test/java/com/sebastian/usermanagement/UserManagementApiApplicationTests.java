896bd1d6bda49e7a3002bef14fa3e52b09602c38
juanse217 <juansebas08004@gmail.com>
2026-02-10 22:34:00 -0500

    chore: commit part of the app

---
+++ src/test/java/com/sebastian/usermanagement/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
